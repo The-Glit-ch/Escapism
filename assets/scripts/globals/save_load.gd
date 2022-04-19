474d52733cb182ed231150aa347ba657cf62ff28
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2022-04-19 19:58:46 -0400

    Backup

---
+++ assets/scripts/globals/save_load.gd
@@ -0,0 +1,12 @@
+class_name ESLS
+extends Node
+
+# Most likely to be changed later on
+#var data_struct_example : Dictionary = {
+#	"global_transform": Transform(),
+#	"obj_type": Node,
+#	"other_data": Dictionary(),
+#}
+
+func _ready():
+	pass
